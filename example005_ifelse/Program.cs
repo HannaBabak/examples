940386045c34cf358e1c667bd78fecbba0334db6
Hanna Babak <kirgiz25@yandex.ru>
2023-11-20 20:34:00 +0300

    blabla

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine()!;
+
+if(username.ToLower() == "Hanna")
+{
+    Console.WriteLine("hey, it's Ann!");
+}
+else
+{    
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
